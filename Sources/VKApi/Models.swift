a028a6bc5bd0602b1fc07acc3a3150c26eee307a
Alex Sed <se-a@mail.ru>
2020-08-02 09:26:45 +0300

    Gate

--- Sources/VKApi/Models.swift
+++ Sources/VKApi/Models.swift
@@ -38,8 +38,8 @@ public struct University: Codable {
 // Bridge to Objective-C
 
 @objc public class ObjCCountry: NSObject {
-    let id: Int
-    let title: String
+    @objc let id: Int
+    @objc let title: String
     @objc public func getTitle() -> String { title }
     @objc public func printTitle() { 
         print("title: \(title)")
